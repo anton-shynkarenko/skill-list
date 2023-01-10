b49b73bfe4bdbfb6330095bbb3b0982056b296f4
Andrii Shynkarenko <shinkarenko.andrii@gmail.com>
2023-01-10 22:37:24 +0200

    7-add-category-entity
    migration added, model added

---
+++ migrations/m230110_201523_create_categories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%categories}}`.
+ */
+class m230110_201523_create_categories_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(125)->notNull(),
+            'description' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%categories}}');
+    }
+}
